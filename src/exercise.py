c3b619a2689f726a0c1196004f4ddf33fd1ce8c7
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 21:50:52 +0000

    added variables and print command for a string.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    #write your code below this line
-
+    #write your code below this line 
+    firstNumber = int(input("Write your first number: "))
+    secondNumber = int(input("Write your second number: "))
+    print(str(firstNumber) + "+" + str(secondNumber) + "=" + str(firstNumber+secondNumber))
+    
+    
+  
 if __name__ == '__main__':
-    main()
+  main()
